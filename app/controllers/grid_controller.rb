7d3abfe51103324482575e9658b50c60aecdb702
SaulGreenberg <sg@10past12.co>
2016-08-16 10:56:27 +0100

    grid game challenge started

---
+++ app/controllers/grid_controller.rb
@@ -0,0 +1,17 @@
+class GridController < ApplicationController
+
+  def generate_grid
+  end
+
+  def included?
+  end
+
+  def compute_score
+  end
+
+  def run_game
+  end
+
+  def score_and_message
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'generate_grid', to: "grid_controller#generate_grid"
+  get ''
+
 end
+
